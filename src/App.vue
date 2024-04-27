89245733d43f4cd1aaa2f51cf3c5b44ae133f249
sean <seanmoore1976@gmail.com>
2024-04-27 13:11:07 -0700

    Set up pinia store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <div>
     <h1>Sean Moore - Software Developer & Technical Writer</h1>
+    {{ appID }}
   </div>
 </template>
+<script setup lang="ts">
+
+import { useMainApplicationStore } from './store'
+import { storeToRefs } from 'pinia'
+
+// access the `store` variable anywhere in the component ✨
+const store = useMainApplicationStore();
+const { appID } = storeToRefs(store)
+
+</script>
---
+++ src/store/index.ts
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+import { ComputedRef, computed } from "vue";
+
+const defineMainApplicationStore = () => {
+  const state = {
+    appID: "seanVue3Website"
+  };
+
+  const getterFunction: ComputedRef<string> = computed(() => "some value" );
+
+  return {
+    ...state,
+    getterFunction
+  };
+};
+
+export const useMainApplicationStore = defineStore('mainApplicationStore', () => {
+  return { ...defineMainApplicationStore() };
+});
