a301e6096bc051a7e3688ece68f5e877a45180e5
sean <seanmoore1976@gmail.com>
2024-05-11 14:52:41 -0700

    refactor: ADd content to About page

--- src/view/About.vue
+++ src/view/About.vue
@@ -2,16 +2,16 @@
   <div class="mb-5 page-title">About</div>
   <v-col cols="12">
     <v-row class="mr-8">
-      <p class="mb-5">Throughout my career, I've functioned as a web, mobile, and desktop developer, technical writer, and reviewer, technical editor,
+      <p class="mb-5">Throughout my career, I've functioned as a web, mobile, and desktop developer, technical writer, and <a href="https://www.oreilly.com/library/view/flex-4-cookbook/9781449388195/pr03.html" target="_blank"> reviewer</a>, technical editor,
         <a href="https://web.archive.org/web/20020325160521/http://lioncorp.net/" target="_blank"> graphic</a>, and UI designer, and journalist.
         I've written on topics ranging from web development to crypto and Web3 to
         <a href="https://a-z-animals.com/author/seanmoore/" target="_blank">lakes</a> and
         <a href="https://www.homemusicmaker.com/fl-studio-ghost-notes" target="_blank"> digital audio production</a>. Some publishers
         I've had the privilege and honor of working with include <a href="https://www.oreilly.com/pub/au/3485" target="_blank">O'Reilly Media</a>,
         Addison-Wesley, Pearson VUE, Adobe, <a href="https://cointelegraph.com/authors/sean-moore" target="_blank">Cointelegraph</a>,
-        Packt Publishing, and Manning.</p>
+        <a href="https://www.packtpub.com/en-ch/product/flash-development-for-android-cookbook-pref04-9781849691420/chapter/about-the-reviewers/about-the-reviewers" target="_blank"> Packt Publishing</a>, and <a href="https://www.manning.com/books/adobe-air-in-action" target="_blank"> Manning</a>.</p>
 
-      <p class="mb-5">I'm currently working with Vue 3, TypeScript, vuetify 3, and pinia, on a SASS application that enables users to manage
+      <p class="mb-5">I'm currently working with Vue 3, TypeScript, vuetify 3, and pinia, on a SASS application that enables users to manage and test
         SCORM conformant e-learning content from the Cloud.</p>
 
       <p class="mb-5">Some companies I've had the utmost privilege and honor of working with as a developer include Adobe, Disney, Microsoft,
@@ -29,9 +29,15 @@
         If you were a developer around 2006-2012 you more than
         likely saw the AS Cheatsheets or maybe even got a laminated one from me at a conference.</p>
 
-      <p class="mb-5">Test driven development (TDD), development best practices, and coding standards are the foundation for a well rounded and
-        sane development experience and have thus been a major focus throughout my career and life. I've written about and given presentations at
-        conferences and user groups on these topics to share what I and many others have learned the hard way.</p>
+      <p class="mb-5">I spent a significant amount of time as an early advocate for source control, TDD, best practices and coding standards. I <a href="https://www.scribd.com/presentation/95139359/Sean-Moore-Flex-Best-Practices" target="_blank"> authored content</a>
+      and provided sessions at multiple conferences and user groups teaching the development community about these topics.</p>
+
+      <p class="mb-5">After many years in public facing, community oriented roles I took a step back and focused on fundamental software development concepts,
+        technical writing and self development. I started my career with a focus on design and gravitated toward development over time. After years of working as a developer,
+        I felt the need to learn more about deeper software engineering principals and topics and also hone my writing skills to improve my overall communication skills and
+        abilities. I also placed too much emphasis on work and the grind in my early career. I took time to really discover who I am and how to better connect with the
+        world around me. If there is one thing I could pass along to younger developers and people just starting a career it would be to mind your mental health and always
+        strive to find and maintain a healthy work life balance. Don't make work your identify, and find hobbies and interests outside of software development.</p>
     </v-row>
   </v-col>
 </template>
