1b7ce4a99e641ddd7d4fffb0039f2107c5b168d0
sean <seanmoore1976@gmail.com>
2024-05-13 18:41:08 -0700

    refactor: Update home page content

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="mb-5 page-title"><v-icon size="25" class="pr-4">mdi-home-outline</v-icon>Home</div>
   <div class="container" style="width: 640px">
-    Welcome to my website. It's always a work in progress. I've made some decent progress over the last few weeks.
-    Take a look around and check out the blog! There's a couple posts in there and more to come.
+    <p class="mb-5">Howdy there! It looks like you've stumbled into my little corner of the web. Welcome.</p>
+
+    <p class="mb-5">I've been a software developer for a lifetime, and I also write. I've also been known to produce
+      art and music from time to time, and I have a plethora of other interests and hobbies. I am the proud owner of a
+      Pomeranian whom I love dearly. He's my second Pom, and wow, these dogs are packed with personality!</p>
+
+    <p class="mb-5">I have worked with various programming languages, stacks, and technologies over the years. Everything
+      from Perl to classic ASP, JSP, JS, TS, Go, Rust, C, Java, Flutter, Vue, Flex, ActionScript, MSSQL, Ingres, MongoDB,
+      MySQL, and many, many others. I'm currently focused on Vue 3, TypeScript, and various associated libraries and
+      tools. One of the first IDEs (editors) I used was BBEdit, and recently, I've been using IntelliJ and VSCode quite
+      often. Way back in olden times, I was an Authorware programmer, and I've worked in e-learning for a good portion
+      of my career. I've also worked as a developer supporting various aspects of movie production, from special effects
+      to websites specifically for movies. I've done some game development (remember Club Penguin?) and also worked on
+      command line applications. I've done quite a bit of UI design and development and find myself doing mostly front
+      end work these days.</p>
+
+    <p class="mb-5">I like to think I'm a pretty interesting human, and there's plenty more I could share. That's probably
+    sufficient for now though. If you have any questions, please get in touch. I'd love to hear from you!</p>
+
   </div>
 </template>
 <script setup lang="ts">
